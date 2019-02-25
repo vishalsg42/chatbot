efb8b21fa2d9bd6593f08baf0e5df60f540cab3e
kevin <kevinj@prdxn.com>
2019-02-25 12:55:49 +0530

    changes in code

--- webhook.js
+++ webhook.js
@@ -127,65 +127,65 @@ app.post('/ai', (req, res) => {
       //  for (i in json.products) {
         //  msg += json.products[i].title + "\n";
          msg += JSON.stringify({
-         "attachment": {
-           "type": "template",
-             "payload": {
-             "template_type": "list",
-               "top_element_style": "compact",
-                 "elements": [
+           attachment: {
+             type: "template",
+             payload: {
+               template_type: "list",
+               top_element_style: "compact",
+               elements: [
                    {
-                     "title": "Classic T-Shirt Collection",
-                     "subtitle": "See all our colors",
-                     "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
-                     "buttons": [
+                   title: "Classic T-Shirt Collection",
+                   subtitle: "See all our colors",
+                   image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                   buttons: [
                        {
-                         "title": "View",
-                         "type": "web_url",
-                         "url": "https://peterssendreceiveapp.ngrok.io/collection",
-                         "messenger_extensions": true,
-                         "webview_height_ratio": "tall",
-                         "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                       title: "View",
+                       type: "web_url",
+                       url: "https://peterssendreceiveapp.ngrok.io/collection",
+                       messenger_extensions: true,
+                       webview_height_ratio: "tall",
+                       fallback_url: "https://peterssendreceiveapp.ngrok.io/"
                        }
                      ]
                    },
                    {
-                     "title": "Classic White T-Shirt",
-                     "subtitle": "See all our colors",
-                     "default_action": {
-                       "type": "web_url",
-                       "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                       "messenger_extensions": false,
-                       "webview_height_ratio": "tall"
+                     title: "Classic White T-Shirt",
+                     subtitle: "See all our colors",
+                     default_action: {
+                       type: "web_url",
+                       url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                       messenger_extensions: false,
+                       webview_height_ratio: "tall"
                      }
                    },
                    {
-                     "title": "Classic Blue T-Shirt",
-                     "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                     "subtitle": "100% Cotton, 200% Comfortable",
-                     "default_action": {
-                       "type": "web_url",
-                       "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                       "messenger_extensions": true,
-                       "webview_height_ratio": "tall",
-                       "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                     title: "Classic Blue T-Shirt",
+                     image_url: "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                     subtitle: "100% Cotton, 200% Comfortable",
+                     default_action: {
+                       type: "web_url",
+                       url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                       messenger_extensions: true,
+                       webview_height_ratio: "tall",
+                       fallback_url: "https://peterssendreceiveapp.ngrok.io/"
                      },
-                     "buttons": [
+                     buttons: [
                        {
-                         "title": "Shop Now",
-                         "type": "web_url",
-                         "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                         "messenger_extensions": true,
-                         "webview_height_ratio": "tall",
-                         "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                         title: "Shop Now",
+                         type: "web_url",
+                         url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                         messenger_extensions: true,
+                         webview_height_ratio: "tall",
+                         fallback_url: "https://peterssendreceiveapp.ngrok.io/"
                        }
                      ]
                    }
                  ],
-                   "buttons": [
+                   buttons: [
                      {
-                       "title": "View More",
-                       "type": "postback",
-                       "payload": "payload"
+                       title: "View More",
+                       type: "postback",
+                       payload: "payload"
                      }
                    ]
            }
