8134afa1a35cab9c76be421489a02be3798c82b0
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-25 12:08:15 +0530

    moving json data to msg variable inside for loop. Trying different approach.

--- webhook.js
+++ webhook.js
@@ -121,79 +121,114 @@ app.post('/ai', (req, res) => {
      if (!err && response.statusCode == 200) {
        let json = JSON.parse(body);
 
-       // Get the name of all products
-      //  let i,j,msg = "";
+      //  Get the name of all products
+       let i,j,msg = "";
 
-      //  for (i in json.products) {
-      //    msg += json.products[i].title + "\n";
-      //  }
-
-      let msg = {
+      //  msg += json.products[i].title + "\n";
+       for (i in json.products) {
+         msg += {
            attachment: {
              type: "template",
-             payload: {
+               payload: {
                template_type: "list",
-               top_element_style: "compact",
-               elements: [
-                 {
-                   title: "Classic T-Shirt Collection",
-                   subtitle: "See all our colors",
-                   image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
-                   buttons: [
+                 top_element_style: "compact",
+                   elements: [
                      {
-                       title: "View",
-                       type: "web_url",
-                       url: "https://peterssendreceiveapp.ngrok.io/collection",
-                       messenger_extensions: true,
-                       webview_height_ratio: "tall",
-                       fallback_url: "https://peterssendreceiveapp.ngrok.io/"
-                     }
-                   ]
-                 },
-                 {
-                   title: "Classic White T-Shirt",
-                   subtitle: "See all our colors",
-                   default_action: {
-                     type: "web_url",
-                     url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                     messenger_extensions: false,
-                     webview_height_ratio: "tall"
-                   }
-                 },
-                 {
-                   title: "Classic Blue T-Shirt",
-                   image_url: "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                   subtitle: "100% Cotton, 200% Comfortable",
-                   default_action: {
-                     type: "web_url",
-                     url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                     messenger_extensions: true,
-                     webview_height_ratio: "tall",
-                     fallback_url: "https://peterssendreceiveapp.ngrok.io/"
-                   },
-                   buttons: [
-                     {
-                       title: "Shop Now",
-                       type: "web_url",
-                       url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                       messenger_extensions: true,
-                       webview_height_ratio: "tall",
-                       fallback_url: "https://peterssendreceiveapp.ngrok.io/"
-                     }
-                   ]
-                 }
-               ],
-               buttons: [
-                 {
-                   title: "View More",
-                   type: "postback",
-                   payload: "payload"
-                 }
-               ]
+                       title: "Classic T-Shirt Collection",
+                       subtitle: "See all our colors",
+                       image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                       buttons: [
+                         {
+                           title: "View",
+                           type: "web_url",
+                           url: "https://peterssendreceiveapp.ngrok.io/collection",
+                           messenger_extensions: true,
+                           webview_height_ratio: "tall",
+                           fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                         }
+                       ]
+                     }                      
+                   ],
+                     buttons: [
+                       {
+                         title: "View More",
+                         type: "postback",
+                         payload: "payload"
+                       }
+                     ]
              }
            }
-          
+
+         };
+
        }
+
+      // let msg = {
+      //      attachment: {
+      //        type: "template",
+      //        payload: {
+      //          template_type: "list",
+      //          top_element_style: "compact",
+      //          elements: [
+      //            {
+      //              title: "Classic T-Shirt Collection",
+      //              subtitle: "See all our colors",
+      //              image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+      //              buttons: [
+      //                {
+      //                  title: "View",
+      //                  type: "web_url",
+      //                  url: "https://peterssendreceiveapp.ngrok.io/collection",
+      //                  messenger_extensions: true,
+      //                  webview_height_ratio: "tall",
+      //                  fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+      //                }
+      //              ]
+      //            },
+      //            {
+      //              title: "Classic White T-Shirt",
+      //              subtitle: "See all our colors",
+      //              default_action: {
+      //                type: "web_url",
+      //                url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
+      //                messenger_extensions: false,
+      //                webview_height_ratio: "tall"
+      //              }
+      //            },
+      //            {
+      //              title: "Classic Blue T-Shirt",
+      //              image_url: "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+      //              subtitle: "100% Cotton, 200% Comfortable",
+      //              default_action: {
+      //                type: "web_url",
+      //                url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
+      //                messenger_extensions: true,
+      //                webview_height_ratio: "tall",
+      //                fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+      //              },
+      //              buttons: [
+      //                {
+      //                  title: "Shop Now",
+      //                  type: "web_url",
+      //                  url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+      //                  messenger_extensions: true,
+      //                  webview_height_ratio: "tall",
+      //                  fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+      //                }
+      //              ]
+      //            }
+      //          ],
+      //          buttons: [
+      //            {
+      //              title: "View More",
+      //              type: "postback",
+      //              payload: "payload"
+      //            }
+      //          ]
+      //        }
+      //      }
+          
+      //  }
 
        return res.send(
          JSON.stringify({
