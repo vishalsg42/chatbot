510776be66dcd385563cbc2b1f3806458394c9c5
kevin <kevinj@prdxn.com>
2019-02-25 12:30:45 +0530

    json key value pairs with doublequotes

--- webhook.js
+++ webhook.js
@@ -128,72 +128,71 @@ app.post('/ai', (req, res) => {
       //    msg += json.products[i].title + "\n";
       //  }
 
-      let msg = {
-           attachment: {
-             type: "template",
-             payload: {
-               template_type: "list",
-               top_element_style: "compact",
-               elements: [
-                 {
-                   title: "Classic T-Shirt Collection",
-                   subtitle: "See all our colors",
-                   image_url: "https://peterssendreceiveapp.ngrok.io/img/collection.png",
-                   buttons: [
-                     {
-                       title: "View",
-                       type: "web_url",
-                       url: "https://peterssendreceiveapp.ngrok.io/collection",
-                       messenger_extensions: true,
-                       webview_height_ratio: "tall",
-                       fallback_url: "https://peterssendreceiveapp.ngrok.io/"
-                     }
-                   ]
-                 },
-                 {
-                   title: "Classic White T-Shirt",
-                   subtitle: "See all our colors",
-                   default_action: {
-                     type: "web_url",
-                     url: "https://peterssendreceiveapp.ngrok.io/view?item=100",
-                     messenger_extensions: false,
-                     webview_height_ratio: "tall"
-                   }
-                 },
-                 {
-                   title: "Classic Blue T-Shirt",
-                   image_url: "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
-                   subtitle: "100% Cotton, 200% Comfortable",
-                   default_action: {
-                     type: "web_url",
-                     url: "https://peterssendreceiveapp.ngrok.io/view?item=101",
-                     messenger_extensions: true,
-                     webview_height_ratio: "tall",
-                     fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+       let msg = {
+         "attachment": {
+           "type": "template",
+             "payload": {
+             "template_type": "list",
+               "top_element_style": "compact",
+                 "elements": [
+                   {
+                     "title": "Classic T-Shirt Collection",
+                     "subtitle": "See all our colors",
+                     "image_url": "https://peterssendreceiveapp.ngrok.io/img/collection.png",
+                     "buttons": [
+                       {
+                         "title": "View",
+                         "type": "web_url",
+                         "url": "https://peterssendreceiveapp.ngrok.io/collection",
+                         "messenger_extensions": true,
+                         "webview_height_ratio": "tall",
+                         "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                       }
+                     ]
                    },
-                   buttons: [
+                   {
+                     "title": "Classic White T-Shirt",
+                     "subtitle": "See all our colors",
+                     "default_action": {
+                       "type": "web_url",
+                       "url": "https://peterssendreceiveapp.ngrok.io/view?item=100",
+                       "messenger_extensions": false,
+                       "webview_height_ratio": "tall"
+                     }
+                   },
+                   {
+                     "title": "Classic Blue T-Shirt",
+                     "image_url": "https://peterssendreceiveapp.ngrok.io/img/blue-t-shirt.png",
+                     "subtitle": "100% Cotton, 200% Comfortable",
+                     "default_action": {
+                       "type": "web_url",
+                       "url": "https://peterssendreceiveapp.ngrok.io/view?item=101",
+                       "messenger_extensions": true,
+                       "webview_height_ratio": "tall",
+                       "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                     },
+                     "buttons": [
+                       {
+                         "title": "Shop Now",
+                         "type": "web_url",
+                         "url": "https://peterssendreceiveapp.ngrok.io/shop?item=101",
+                         "messenger_extensions": true,
+                         "webview_height_ratio": "tall",
+                         "fallback_url": "https://peterssendreceiveapp.ngrok.io/"
+                       }
+                     ]
+                   }
+                 ],
+                   "buttons": [
                      {
-                       title: "Shop Now",
-                       type: "web_url",
-                       url: "https://peterssendreceiveapp.ngrok.io/shop?item=101",
-                       messenger_extensions: true,
-                       webview_height_ratio: "tall",
-                       fallback_url: "https://peterssendreceiveapp.ngrok.io/"
+                       "title": "View More",
+                       "type": "postback",
+                       "payload": "payload"
                      }
                    ]
-                 }
-               ],
-               buttons: [
-                 {
-                   title: "View More",
-                   type: "postback",
-                   payload: "payload"
-                 }
-               ]
-             }
            }
-          
-       }
+         }
+        }
 
        return res.send(
          JSON.stringify({
