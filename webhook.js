a3b6da10a26dba1f8e5ab7a40d20b18759253ed9
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-26 11:51:23 +0530

    changes made in code

--- webhook.js
+++ webhook.js
@@ -125,40 +125,40 @@ app.post('/ai', (req, res) => {
       //  Get the name of all products
        let i,msg = "";
 
-      //  for (i in json.products) {
-        //  msg += json.products[i].title + "\n";
+       for (i in json.products) {
+         msg += json.products[i].title + "\n";
         
-       msg += [JSON.stringify({
-           "attachment": {
-             "type": "template",
-             "payload": {
-               "template_type": "generic",
-               "elements": [
-                 {
-                   "title": "Welcome!",
-                   "image_url": "https://petersfancybrownhats.com/company_image.png",
-                   "subtitle": "We have the right hat for everyone.",
-                   "default_action": {
-                     "type": "web_url",
-                     "url": "https://petersfancybrownhats.com/view?item=103",
-                     "webview_height_ratio": "tall",
-                   },
-                   "buttons": [
-                     {
-                       "type": "web_url",
-                       "url": "https://petersfancybrownhats.com",
-                       "title": "View Website"
-                     }, {
-                       "type": "postback",
-                       "title": "Start Chatting",
-                       "payload": "DEVELOPER_DEFINED_PAYLOAD"
-                     }
-                   ]
-                 }
-               ]
-             }
-           } 
-       })] 
+      //  msg += [JSON.stringify({
+      //      "attachment": {
+      //        "type": "template",
+      //        "payload": {
+      //          "template_type": "generic",
+      //          "elements": [
+      //            {
+      //              "title": "Welcome!",
+      //              "image_url": "https://petersfancybrownhats.com/company_image.png",
+      //              "subtitle": "We have the right hat for everyone.",
+      //              "default_action": {
+      //                "type": "web_url",
+      //                "url": "https://petersfancybrownhats.com/view?item=103",
+      //                "webview_height_ratio": "tall",
+      //              },
+      //              "buttons": [
+      //                {
+      //                  "type": "web_url",
+      //                  "url": "https://petersfancybrownhats.com",
+      //                  "title": "View Website"
+      //                }, {
+      //                  "type": "postback",
+      //                  "title": "Start Chatting",
+      //                  "payload": "DEVELOPER_DEFINED_PAYLOAD"
+      //                }
+      //              ]
+      //            }
+      //          ]
+      //        }
+      //      } 
+      //  })] 
         console.log(msg)
 
       //  let msg = {
