27f639be7b83ee97ef4f7b2e4aa883c5529c3a52
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-26 12:34:21 +0530

    changes made in code

--- webhook.js
+++ webhook.js
@@ -163,7 +163,7 @@ app.post('/ai', (req, res) => {
 
        return res.send(
          JSON.stringify({
-           parameters: msg,
+           fulfillmentMessages: msg,
            source: 'productList'
           })
        );
