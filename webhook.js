fcd22825e884750278f97be180a7186fe418ef32
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-26 13:25:10 +0530

    changes made in code

--- webhook.js
+++ webhook.js
@@ -143,6 +143,15 @@ app.post('/ai', (req, res) => {
                      "url": "https://petersfancybrownhats.com/view?item=103",
                      "webview_height_ratio": "tall",
                    },
+                   {
+                   "title": "Welcome again!",
+                   "image_url": "https://petersfancybrownhats.com/company_image.png",
+                   "subtitle": "We have the right hat for everyone.",
+                   "default_action": {
+                     "type": "web_url",
+                     "url": "https://petersfancybrownhats.com/view?item=103",
+                     "webview_height_ratio": "tall",
+                   },
                    "buttons": [
                      {
                        "type": "web_url",
