f15db8618b187888004eb7785e991cbcd65137c2
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-26 20:14:28 +0530

    removed JSON.stringify

--- webhook.js
+++ webhook.js
@@ -76,7 +76,7 @@ function sendMessage(event) {
 
  apiai.on('response', (response) => {
    console.log(response)
-   let aiText = response.result.fulfillment.speech;
+  //  let aiText = response.result.fulfillment.speech;
 
    request({
      url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -84,7 +84,8 @@ function sendMessage(event) {
      method: 'POST',
      json: {
        recipient: { id: sender },
-       message: { text: aiText }
+      //  message: { text: aiText }
+      message: response.result
       }
     }, (error, response) => {
       if (error) {
