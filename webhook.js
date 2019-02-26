a18ac536f89b32076bfb0241adedc2cb52e3c3c7
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-26 19:26:27 +0530

    Rich messages code

--- webhook.js
+++ webhook.js
@@ -127,45 +127,62 @@ app.post('/ai', (req, res) => {
 
       //  for (i in json.products) {
         //  msg += json.products[i].title + "\n";
-        
-       msg += JSON.stringify({
-           "attachment": {
-             "type": "template",
-             "payload": {
-               "template_type": "generic",
-               "elements": [
-                 {
-                   "title": "Welcome!",
-                   "image_url": "https://petersfancybrownhats.com/company_image.png",
-                   "subtitle": "We have the right hat for everyone.",
-                   "default_action": {
-                     "type": "web_url",
-                     "url": "https://petersfancybrownhats.com/view?item=103",
-                     "webview_height_ratio": "tall",
-                   },
-                   "buttons": [
-                     {
-                       "type": "web_url",
-                       "url": "https://petersfancybrownhats.com",
-                       "title": "View Website"
-                     }, {
-                       "type": "postback",
-                       "title": "Start Chatting",
-                       "payload": "DEVELOPER_DEFINED_PAYLOAD"
-                     }
-                   ]
-                 }
-               ]
-             }
-           } 
-       })
-        console.log(msg)
+
+       let Richresponse = [
+         {
+           "card": {
+             "title": "Flower",
+             "subtitle": "Red Flower",
+             "imageUri": "https://firebasestorage.googleapis.com/v0/b/agent-anonym.appspot.com/o/flower1.jpg?alt=media&token=b3342402-855f-416c-a486-72a631350e7f",
+             "buttons": [
+               {
+                 "text": "Download"
+               },
+               {
+                 "text": "Visit"
+               }
+             ]
+           },
+           "platform": "FACEBOOK"
+         }
+       ]
+      //      "attachment": {
+      //        "type": "template",
+      //        "payload": {
+      //          "template_type": "generic",
+      //          "elements": [
+      //            {
+      //              "title": "Welcome!",
+      //              "image_url": "https://petersfancybrownhats.com/company_image.png",
+      //              "subtitle": "We have the right hat for everyone.",
+      //              "default_action": {
+      //                "type": "web_url",
+      //                "url": "https://petersfancybrownhats.com/view?item=103",
+      //                "webview_height_ratio": "tall",
+      //              },
+      //              "buttons": [
+      //                {
+      //                  "type": "web_url",
+      //                  "url": "https://petersfancybrownhats.com",
+      //                  "title": "View Website"
+      //                }, {
+      //                  "type": "postback",
+      //                  "title": "Start Chatting",
+      //                  "payload": "DEVELOPER_DEFINED_PAYLOAD"
+      //                }
+      //              ]
+      //            }
+      //          ]
+      //        }
+      //      } 
+      //  })
+        console.log(Richresponse)
 
        return res.send(
-         JSON.stringify({
-           fulfillmentText: msg,
-           source: 'productList'
-          })
+        //  JSON.stringify({
+           fulfillmentMessages = Richresponse
+          //  source: 'productList'
+          // })
        );
 
      } else {
