8f105d87439439bded1e436780c7504eaa568c8c
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-27 16:10:20 +0530

    added facebook code

--- webhook.js
+++ webhook.js
@@ -130,63 +130,23 @@ app.post('/ai', (req, res) => {
 
         msg = [
           {
-            "message": {
-              "attachment": {
-                "type": "template",
-                "payload": {
-                  "template_type": "receipt",
-                  "recipient_name": "Stephane Crozatier",
-                  "order_number": "12345678902",
-                  "currency": "USD",
-                  "payment_method": "Visa 2345",
-                  "order_url": "https://firebasestorage.googleapis.com/v0/b/agent-anonym.appspot.com/o/flower4.jpg?alt=media&token=0efc8cf4-a054-4efb-863d-884f3608a671",
-                  "timestamp": "1428444852",
-                  "address": {
-                    "street_1": "1 Hacker Way",
-                    "street_2": "",
-                    "city": "Menlo Park",
-                    "postal_code": "94025",
-                    "state": "CA",
-                    "country": "US"
-                  },
-                  "summary": {
-                    "subtotal": 75.00,
-                    "shipping_cost": 4.95,
-                    "total_tax": 6.19,
-                    "total_cost": 56.14
-                  },
-                  "adjustments": [
-                    {
-                      "name": "New Customer Discount",
-                      "amount": 20
-                    },
-                    {
-                      "name": "$10 Off Coupon",
-                      "amount": 10
-                    }
-                  ],
-                  "elements": [
-                    {
-                      "title": "Classic White T-Shirt",
-                      "subtitle": "100% Soft and Luxurious Cotton",
-                      "quantity": 2,
-                      "price": 50,
-                      "currency": "USD",
-                      "image_url": "https://firebasestorage.googleapis.com/v0/b/agent-anonym.appspot.com/o/flower4.jpg?alt=media&token=0efc8cf4-a054-4efb-863d-884f3608a671"
-                    },
-                    {
-                      "title": "Classic Gray T-Shirt",
-                      "subtitle": "100% Soft and Luxurious Cotton",
-                      "quantity": 1,
-                      "price": 25,
-                      "currency": "USD",
-                      "image_url": "https://firebasestorage.googleapis.com/v0/b/agent-anonym.appspot.com/o/flower4.jpg?alt=media&token=0efc8cf4-a054-4efb-863d-884f3608a671"
-                    }
-                  ]
+            "card": {
+              "title": "Flower",
+              "subtitle": "Red Flower",
+              "imageUri": "https://firebasestorage.googleapis.com/v0/b/agent-anonym.appspot.com/o/flower4.jpg?alt=media&token=0efc8cf4-a054-4efb-863d-884f3608a671",
+              "buttons": [
+                {
+                  "text": "Visit Google",
+                  "postback": "www.google.com"
+                },
+                {
+                  "text": "Visit Dialogflow",
+                  "postback": "www.dialogflow.com"
                 }
-              }
+              ]
+            },
+            "platform": "FACEBOOK"
           }
-        }
         ]
 
         console.log(msg)
