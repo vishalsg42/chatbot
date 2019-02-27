c6815782ca2ceaafc183d55053fda1a4afa76eb9
kevin <kevinj@prdxn.com>
2019-02-27 15:57:27 +0530

    conflict solved

--- webhook.js
+++ webhook.js
@@ -69,14 +69,11 @@ app.post('/webhook', (req, res) => {
 function sendMessage(event) {
   let sender = event.sender.id;
   let text = event.message.text;
-<<<<<<< HEAD
   let attachment = event.message.attachments;
 
   //  let apiai = apiaiApp.textRequest(text, {
   //    sessionId: 'shopify_bot'
   //  });
-=======
->>>>>>> f22d79be13ac9d5b610f0e6e2b18b20dcb4aeae9
 
   let apiai = apiaiApp.textRequest(text, {
     sessionId: 'shopify_bot'
@@ -101,6 +98,7 @@ function sendMessage(event) {
         console.log('Error: ', response.body.error);
       }
     });
+    console.log('TCL: sendMessage -> request', request);
   });
 
   apiai.on('error', (error) => {
@@ -129,73 +127,17 @@ app.post('/ai', (req, res) => {
       if (!err && response.statusCode == 200) {
         let json = JSON.parse(body);
 
-<<<<<<< HEAD
         //  Get the name of all products
         let i, msg = "";
 
         for (i in json.products) {
           msg += json.products[i].title + "\n";
         }
-=======
-        // Get the name of all products
-        let i, j, msg = "";
-
-        // for (i in json.products) {
-        //   msg += json.products[i].title + "\n";
-        // }
-
-        // msg = [
-        //   {
-        //     "card": {
-        //       "title": "Flower",
-        //       "subtitle": "Red Flower",
-        //       "imageUri": "https://firebasestorage.googleapis.com/v0/b/agent-anonym.appspot.com/o/flower1.jpg?alt=media&token=b3342402-855f-416c-a486-72a631350e7f",
-        //       "buttons": [
-        //         {
-        //           "text": "Visit Google",
-        //           "postback": "www.google.com"
-        //         },
-        //         {
-        //           "text": "Visit Dialogflow",
-        //           "postback": "www.dialogflow.com"
-        //         }
-        //       ]
-        //     },
-        //     "platform": "FACEBOOK"
-        //   }
-        // ]
-
-        msg = [
-          {
-            "card": {
-              "title": "Flower",
-              "subtitle": "Red Flower",
-              "imageUri": "https://firebasestorage.googleapis.com/v0/b/agent-anonym.appspot.com/o/flower1.jpg?alt=media&token=b3342402-855f-416c-a486-72a631350e7f",
-              "buttons": [
-                {
-                  "text": "Visit Google",
-                  "postback": "www.google.com"
-                },
-                {
-                  "text": "Visit Dialogflow",
-                  "postback": "www.dialogflow.com"
-                }
-              ]
-            },
-            "platform": "FACEBOOK"
-          }
-        ]
-
->>>>>>> f22d79be13ac9d5b610f0e6e2b18b20dcb4aeae9
         console.log(msg)
 
         return res.send(
           JSON.stringify({
-<<<<<<< HEAD
             fulfillmentText: msg,
-=======
-            fulfillmentMessages: msg,
->>>>>>> f22d79be13ac9d5b610f0e6e2b18b20dcb4aeae9
             source: 'productList'
           })
         );
@@ -211,7 +153,6 @@ app.post('/ai', (req, res) => {
       }
     })
   }
-<<<<<<< HEAD
 });
 
 // card
@@ -223,6 +164,3 @@ console.log('TCL: agent', agent)
 
 
 agent.add(`Check this out`);
-=======
-});
->>>>>>> f22d79be13ac9d5b610f0e6e2b18b20dcb4aeae9
