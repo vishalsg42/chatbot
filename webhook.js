f29caa38d37c457204cf65d087fc7adc1bab9a2e
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-27 15:37:11 +0530

    added facebook code

--- webhook.js
+++ webhook.js
@@ -156,25 +156,22 @@ app.post('/ai', (req, res) => {
           "template_type": "generic",
             "elements": [
               {
-                "title": "Welcome!",
-                "image_url": "https://petersfancybrownhats.com/company_image.png",
-                "subtitle": "We have the right hat for everyone.",
-                "default_action": {
-                  "type": "web_url",
-                  "url": "https://petersfancybrownhats.com/view?item=103",
-                  "webview_height_ratio": "tall",
+                "card": {
+                  "title": "Flower",
+                  "subtitle": "Red Flower",
+                  "imageUri": "https://firebasestorage.googleapis.com/v0/b/agent-anonym.appspot.com/o/flower1.jpg?alt=media&token=b3342402-855f-416c-a486-72a631350e7f",
+                  "buttons": [
+                    {
+                      "text": "Visit Google",
+                      "postback": "www.google.com"
+                    },
+                    {
+                      "text": "Visit Dialogflow",
+                      "postback": "www.dialogflow.com"
+                    }
+                  ]
                 },
-                "buttons": [
-                  {
-                    "type": "web_url",
-                    "url": "https://petersfancybrownhats.com",
-                    "title": "View Website"
-                  }, {
-                    "type": "postback",
-                    "title": "Start Chatting",
-                    "payload": "DEVELOPER_DEFINED_PAYLOAD"
-                  }
-                ]
+                "platform": "FACEBOOK"
               }
             ]
         }
