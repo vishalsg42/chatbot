5fa3081c39f9c597d3a58eed1b21f8e0a1431521
kevin <kevinj@prdxn.com>
2019-02-27 15:51:49 +0530

    Added new library code

--- webhook.js
+++ webhook.js
@@ -11,7 +11,7 @@
 const PAGE_ACCESS_TOKEN = 'EAAEWKehLl14BACyrHkqqsUoUoZBylHsmg5Pf9B0A4BgB021QkaojpENup2SwpWSfRNHfulmccqG3CcFpNyAudzhWUIk7rQeQ6x0kTYlpWrdZBLyymemja4ZAxujh4mOVp7SthzihYQSYutGjhwtwqViRvp14XJeruO3xAxubgZDZD';
 
 // APIAI_TOKEN = 'dialogflow_client_access_token'
-const APIAI_TOKEN ='7266df4034974d5fa42d7c10b02fef32';
+const APIAI_TOKEN = '7266df4034974d5fa42d7c10b02fef32';
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -26,71 +26,70 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 // // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 
 // Process application/json
 app.use(bodyParser.json())
 
 
 const server = app.listen(process.env.PORT || 5000, () => {
- console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
+  console.log('Express server listening on port %d in %s mode', server.address().port, app.settings.env);
 });
 
 // webhook route
 app.get('/', function (req, res) {
- res.send('Hello world, I am a chat bot')
+  res.send('Hello world, I am a chat bot')
 })
 
 /* For Facebook Validation */
 app.get('/webhook', (req, res) => {
- if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'shopify-messenger-integration') {
-   res.status(200).send(req.query['hub.challenge']);
- } else {
-   res.status(403).end();
- }
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'shopify-messenger-integration') {
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    res.status(403).end();
+  }
 });
 
 /* Handling all messenges */
 app.post('/webhook', (req, res) => {
- if (req.body.object === 'page') {
-   req.body.entry.forEach((entry) => {
-     entry.messaging.forEach((event) => {
-       if (event.message && event.message.text) {
-         sendMessage(event);
-       }
-     });
-   });
-   res.status(200).end();
- }
+  if (req.body.object === 'page') {
+    req.body.entry.forEach((entry) => {
+      entry.messaging.forEach((event) => {
+        if (event.message && event.message.text) {
+          sendMessage(event);
+        }
+      });
+    });
+    res.status(200).end();
+  }
 });
 
 /* GET query from API.ai */
 
 function sendMessage(event) {
- let sender = event.sender.id;
- let text = event.message.text;
- let attachment = event.message.attachments;
+  let sender = event.sender.id;
+  let text = event.message.text;
+  let attachment = event.message.attachments;
 
-//  let apiai = apiaiApp.textRequest(text, {
-//    sessionId: 'shopify_bot'
-//  });
+  //  let apiai = apiaiApp.textRequest(text, {
+  //    sessionId: 'shopify_bot'
+  //  });
 
-  let apiai = apiaiApp.textRequest(attachment, {
+  let apiai = apiaiApp.textRequest(text, {
     sessionId: 'shopify_bot'
   });
 
- apiai.on('response', (response) => {
-   console.log(response)
-   let aiText = response.result.fulfillment.messages;
+  apiai.on('response', (response) => {
+    console.log(response)
+    let aiText = response.result.fulfillment.speech;
 
-   request({
-     url: 'https://graph.facebook.com/v2.6/me/messages',
-     qs: {access_token: PAGE_ACCESS_TOKEN},
-     method: 'POST',
-     json: {
-       recipient: { id: sender },
-       message: { attachment: aiText }
-      // message: messageData,
+    request({
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: { access_token: PAGE_ACCESS_TOKEN },
+      method: 'POST',
+      json: {
+        recipient: { id: sender },
+        message: { text: aiText }
       }
     }, (error, response) => {
       if (error) {
@@ -102,166 +101,66 @@ function sendMessage(event) {
     console.log('TCL: sendMessage -> request', request);
   });
 
- apiai.on('error', (error) => {
-   console.log(error);
- });
+  apiai.on('error', (error) => {
+    console.log(error);
+  });
 
- apiai.end();
+  apiai.end();
 }
 
 /* Webhook for API.ai to get response from the 3rd party API */
 app.post('/ai', (req, res) => {
   /* Shopify API call for product */
   if (req.body.queryResult.action === 'productList') {
-   console.log('*** product ***');
+    console.log('*** product ***');
 
-   // username = 'shopify_app_username'
-   const username = '8e68b0ada51356f5fe79a3bb660133de';
-   // password = 'shopify_app_password'
-   const password = 'ca1e7b2a49df493443c15f5b5201ee50';
-   // shop = 'shop_name'
-   const shop = 'internal-example-store';
+    // username = 'shopify_app_username'
+    const username = '8e68b0ada51356f5fe79a3bb660133de';
+    // password = 'shopify_app_password'
+    const password = 'ca1e7b2a49df493443c15f5b5201ee50';
+    // shop = 'shop_name'
+    const shop = 'internal-example-store';
 
-   let restUrl = 'https://'+username+':'+password+'@'+shop+'.myshopify.com/admin/products.json';
+    let restUrl = 'https://' + username + ':' + password + '@' + shop + '.myshopify.com/admin/products.json';
 
-   request.get(restUrl, (err, response, body) => {
-     if (!err && response.statusCode == 200) {
-       let json = JSON.parse(body);
+    request.get(restUrl, (err, response, body) => {
+      if (!err && response.statusCode == 200) {
+        let json = JSON.parse(body);
 
-      //  Get the name of all products
-      //  let i,msg = "";
+        //  Get the name of all products
+        let i, msg = "";
 
-      //  for (i in json.products) {
-        //  msg += json.products[i].title + "\n";
+        for (i in json.products) {
+          msg += json.products[i].title + "\n";
+        }
+        console.log(msg)
 
-      //  msg = [
-        //  {
-          //  "card": {
-          //    "title": "Flower",
-          //    "subtitle": "Red Flower",
-          //    "imageUri": "https://firebasestorage.googleapis.com/v0/b/agent-anonym.appspot.com/o/flower1.jpg?alt=media&token=b3342402-855f-416c-a486-72a631350e7f",
-          //    "buttons": [
-          //      {
-          //        "text": "Download"
-          //      },
-          //      {
-          //        "text": "Visit"
-          //      }
-          //    ]
-          //  },
-          //  "platform": "FACEBOOK"
-      //      "attachment": {
-      //        "type": "template",
-      //        "payload": {
-      //          "template_type": "generic",
-      //          "elements": [
-      //            {
-      //              "title": "Welcome!",
-      //              "image_url": "https://petersfancybrownhats.com/company_image.png",
-      //              "subtitle": "We have the right hat for everyone.",
-      //              "default_action": {
-      //                "type": "web_url",
-      //                "url": "https://petersfancybrownhats.com/view?item=103",
-      //                "webview_height_ratio": "tall",
-      //              },
-      //              "buttons": [
-      //                {
-      //                  "type": "web_url",
-      //                  "url": "https://petersfancybrownhats.com",
-      //                  "title": "View Website"
-      //                }, {
-      //                  "type": "postback",
-      //                  "title": "Start Chatting",
-      //                  "payload": "DEVELOPER_DEFINED_PAYLOAD"
-      //                }
-      //              ]
-      //            }
-      //          ]
-      //        }
-      //      } 
-      //    }
-      //  ]
-      //      "attachment": {
-      //        "type": "template",
-      //        "payload": {
-      //          "template_type": "generic",
-      //          "elements": [
-      //            {
-      //              "title": "Welcome!",
-      //              "image_url": "https://petersfancybrownhats.com/company_image.png",
-      //              "subtitle": "We have the right hat for everyone.",
-      //              "default_action": {
-      //                "type": "web_url",
-      //                "url": "https://petersfancybrownhats.com/view?item=103",
-      //                "webview_height_ratio": "tall",
-      //              },
-      //              "buttons": [
-      //                {
-      //                  "type": "web_url",
-      //                  "url": "https://petersfancybrownhats.com",
-      //                  "title": "View Website"
-      //                }, {
-      //                  "type": "postback",
-      //                  "title": "Start Chatting",
-      //                  "payload": "DEVELOPER_DEFINED_PAYLOAD"
-      //                }
-      //              ]
-      //            }
-      //          ]
-      //        }
-      //      } 
-      //  })
-      //  console.log(msg)
-       let messageData = [{
-         "attachment": {
-           "type": "template",
-           "payload": {
-             "template_type": "generic",
-             "elements": [{
-               "title": "First card",
-               "subtitle": "Element #1 of an hscroll",
-               "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-               "buttons": [{
-                 "type": "web_url",
-                 "url": "https://www.messenger.com",
-                 "title": "web url"
-               }, {
-                 "type": "postback",
-                 "title": "Postback",
-                 "payload": "Payload for first element in a generic bubble",
-               }],
-             }, {
-               "title": "Second card",
-               "subtitle": "Element #2 of an hscroll",
-               "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-               "buttons": [{
-                 "type": "postback",
-                 "title": "Postback",
-                 "payload": "Payload for second element in a generic bubble",
-               }],
-             }]
-           }
-         }
-        }]
-
-       return res.send(
-         JSON.stringify({
-          //  fulfillmentText: "msg",  
-           fulfillmentMessages: messageData,
-           source: 'productList'
+        return res.send(
+          JSON.stringify({
+            fulfillmentText: msg,
+            source: 'productList'
           })
-          // messageData
-       );
+        );
 
-     } else {
-       let errorMessage = 'I failed to look up the product name.';
-       return res.status(400).json({
-         status: {
-           code: 400,
-           errorType: errorMessage
-         }
-       });
-     }
-   })
- }
+      } else {
+        let errorMessage = 'I failed to look up the product name.';
+        return res.status(400).json({
+          status: {
+            code: 400,
+            errorType: errorMessage
+          }
+        });
+      }
+    })
+  }
 });
+
+// card
+const { WebhookClient } = require('dialogflow-fulfillment');
+const { Card } = require('dialogflow-fulfillment');
+
+const agent = new WebhookClient({ request, response });
+console.log('TCL: agent', agent)
+
+
+agent.add(`Check this out`);
