07bdb8f56cd63edda1679f311102aadd91f4e198
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-28 11:04:00 +0530

    code changed

--- webhook.js
+++ webhook.js
@@ -131,27 +131,40 @@ app.post('/ai', (req, res) => {
         // }
 
         msg = [
-          {
-            "card": {
-              "title": "Flower",
-              "subtitle": "Red Flower",
-              "imageUri": "https://firebasestorage.googleapis.com/v0/b/agent-anonym.appspot.com/o/flower1.jpg?alt=media&token=b3342402-855f-416c-a486-72a631350e7f",
-              "buttons": [
-                {
-                  "text": "Visit Google",
-                  "postback": "www.google.com"
-                },
-                {
-                  "text": "Visit Dialogflow",
-                  "postback": "www.dialogflow.com"
-                }
-              ]
-            },
-            "platform": "FACEBOOK"
+          {            
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [
+                  {
+                    "title": "Welcome!",
+                    "image_url": "https://petersfancybrownhats.com/company_image.png",
+                    "subtitle": "We have the right hat for everyone.",
+                    "default_action": {
+                      "type": "web_url",
+                      "url": "https://petersfancybrownhats.com/view?item=103",
+                      "webview_height_ratio": "tall",
+                    },
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "url": "https://petersfancybrownhats.com",
+                        "title": "View Website"
+                      }, {
+                        "type": "postback",
+                        "title": "Start Chatting",
+                        "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                      }
+                    ]
+                  }
+                ]
+              }
+            }            
           }
         ]
 
-        console.log(msg)
+        console.log(msg);
 
         return res.send(
           JSON.stringify({
