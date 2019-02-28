83ac143f22425ae9dca09ca07ff66742c474595d
ajinkyagprdxn <ajinkyag@prdxn.com>
2019-02-28 10:13:44 +0530

    code changed

--- webhook.js
+++ webhook.js
@@ -84,7 +84,38 @@ function sendMessage(event) {
       method: 'POST',
       json: {
         recipient: { id: sender },
-        message: { text: aiText }
+        // message: { text: aiText }        
+          "message": {
+            "attachment": {
+              "type": "template",
+              "payload": {
+                "template_type": "generic",
+                "elements": [
+                  {
+                    "title": "Welcome!",
+                    "image_url": "https://petersfancybrownhats.com/company_image.png",
+                    "subtitle": "We have the right hat for everyone.",
+                    "default_action": {
+                      "type": "web_url",
+                      "url": "https://petersfancybrownhats.com/view?item=103",
+                      "webview_height_ratio": "tall",
+                    },
+                    "buttons": [
+                      {
+                        "type": "web_url",
+                        "url": "https://petersfancybrownhats.com",
+                        "title": "View Website"
+                      }, {
+                        "type": "postback",
+                        "title": "Start Chatting",
+                        "payload": "DEVELOPER_DEFINED_PAYLOAD"
+                      }
+                    ]
+                  }
+                ]
+              }
+            }
+          }
       }
     }, (error, response) => {
       if (error) {
